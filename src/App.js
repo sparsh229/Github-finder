36121ccff41586a8671ade7d0fad8759531a6b06
sparsh229 <sparsh.gupta229@gmail.com>
2021-05-27 23:32:09 +0530

    woking website with add  search button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import "./App.css";
-import React, { Fragment } from "react";
+import React, { Component } from "react";
 import Navbar from "./Component/Layout/Navbar";
 import Users from "./Component/Users/Users";
-const App = () => {
-  return (
-    <Fragment>
-      <Navbar />
-      <div className="container">
-        <Users />
+import Search from "./Component/Users/Search";
+import axios from "axios";
+class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data.items, loading: false });
+  };
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Search searchUsers={this.searchUsers} />
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
-    </Fragment>
-  );
-};
+    );
+  }
+}
 export default App;
--- src/Component/Users/Users.js
+++ src/Component/Users/Users.js
@@ -1,38 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import Useritem from "./Useritem";
-export class Users extends Component {
-  state = {
-    users: [
-      {
-        id: "id1",
-        login: "Sparsh Gupta",
-        avatar_url: "https://avatars.githubusercontent.com/u/71493667?v=4",
-        html_url: "https://github.com/sparsh229",
-      },
-      {
-        id: "id2",
-        login: "Sparsh Gupta",
-        avatar_url: "https://avatars.githubusercontent.com/u/71493667?v=4",
-        html_url: "https://github.com/sparsh229",
-      },
-      {
-        id: "id3",
-        login: "Sparsh Gupta",
-        avatar_url: "https://avatars.githubusercontent.com/u/71493667?v=4",
-        html_url: "https://github.com/sparsh229",
-      },
-    ],
-  };
-  render() {
+import Spinner from "../Layout/Spinner";
+import PropTypes from "prop-types";
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userDisplay}>
-        {this.state.users.map((user) => (
+        {users.map((user) => (
           <Useritem key={user.id} user={user} />
         ))}
       </div>
     );
   }
-}
+};
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
 const userDisplay = {
   display: "grid",
   gridTemplateColumns: "repeat(3,1fr)",
